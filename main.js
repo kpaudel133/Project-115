b0c504257df8da539ebbaecd78292f55c2e98f8e
kpaudel133 <75652036+kpaudel133@users.noreply.github.com>
2022-05-22 10:23:02 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+function preload(){
+}
+
+function setup(){
+    canvas = createCanvas(300,300);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on("pose", gotPoses);
+}
+
+function draw(){
+    image(video, 0, 0, 300, 300);
+}
+
+function takeSnapshot(){
+    save('myFilterImage.png');
+}
+
+function modelLoaded(){
+    console.log("PoseNet is Initialized")
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        console.log("Right eye x = " + results[0].pose.rightEye.x);
+        console.log("Right eye y = " + results[0].pose.rightEye.y);
+        console.log("Left eye x = " + results[0].pose.leftEye.x);
+        console.log("Left eye y = " + results[0].pose.leftEye.y);
+    }
+}
